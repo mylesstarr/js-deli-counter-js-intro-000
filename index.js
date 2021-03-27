ad7978e8a22ab934cb8ed94b40e4fb1e6f1fc76d
Myles Starr <mylesstarr@gmail.com>
2021-03-27 19:30:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,3 +29,18 @@ function currentLine(line) {
    * into a String. */
   return `The line is currently: ${numbersAndNames.join(', ')}`
 };
+
+
+function currentLine(line) {
+  if (!line.legnth) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+     numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
